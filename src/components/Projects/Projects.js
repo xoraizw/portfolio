747d35294d07462dddeda17cee0be409fd023e0d
Ahmad Xoraiz Waheed <xoraizw@gmail.com>
2025-02-23 14:44:53 +0500

    Updated portfolio with new changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,6 +8,7 @@ import sugarrush from "../../Assets/sugar-rush.png";
 import movieSniper from "../../Assets/movieSniper.png"
 import ml from "../../Assets/ml.png"
 import police from "../../Assets/police.png"
+import quant from "../../Assets/quant.png"
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -64,7 +65,24 @@ function Projects() {
               ghLink="https://github.com/xoraizw/Point-cloud-segmentation-for-road-damage-detection"
             />
           </Col>
-
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={quant}
+              isBlog={false}
+              title="Optimizing Deep Learning Models with Quantization"
+              description="Explore Post-Training Quantization (PTQ) and Quantization-Aware Training (QAT) to reduce memory and computational demand without sacrificing performance. Dive into precision trade-offs, bit-width evaluations, and scaling law analysis, all using PyTorch and CIFAR-100 for edge-ready AI models."
+              ghLink="https://github.com/xoraizw/Optimizing-Deep-Learning-with-Quantization"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ml}
+              isBlog={false}
+              title="Sentiment Prediction Model for Wikipedia Comments"
+              description="Built a toxic comment classification model using LSTM, Naïve Bayes, and Sentence Transformers, achieving up to 98% accuracy for enhanced content moderation."
+              ghLink="https://github.com/ahsanjahangirmir/toxicity-classification-challenge"
+            />
+          </Col>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={police}
@@ -72,16 +90,6 @@ function Projects() {
               title="Predicting Police Violence Trends Using Machine Learning"
               description="Developed a ML model by cleaning, processing and analyzing 10+ years of U.S. police violence data, predicting cause of death & racial disparities with 80%+ accuracy. Used Logistic Regression, Random Forest, and SMOTE to handle imbalance, improving fairness insights for policy discussions."
               ghLink="https://medium.com/@abdulhaseeb494/modelling-the-mapping-police-violence-dataset-3f8754e5045b"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={ml}
-              isBlog={false}
-              title="Sentiment Prediction Model for Wikipedia Comments"
-              description="●	Built a toxic comment classification model using LSTM, Naïve Bayes, and Sentence Transformers, achieving up to 98% accuracy for enhanced content moderation."
-              ghLink="https://github.com/ahsanjahangirmir/toxicity-classification-challenge"
             />
           </Col>
         </Row>
